b7cdfe088fb4b5cd79bdf17ea646b1b5c8b651e8
baxtiyorov2010 <torabaxtiyorov29@gmail.com>
2024-08-21 15:45:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let ism = prompt("Ismingizni kiriting")
+let familya = prompt("familyangizni kiriting")
+let yosh = prompt("yoshingizni kiriting")
+let subject = prompt("Yoqtirgan faningizni yozig")
+let contry = prompt("Yashaydigan tumaningizni kiriting")
+
+console.log(`ismingiz:  ${ism} ` )
+console.log(`familyangiz:  ${familya}`);
+console.log(`yoshingiz:  ${yosh}`);
+console.log(`yashaydigan manzilingiz:  ${contry}`);
+
+alert("javob berganingiz uchun raxmat")
